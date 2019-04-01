634e5d7f971fc2913ea7c2b755c7e02a8e2e5d45
Darth .G <45967111+DavidG8168@users.noreply.github.com>
2019-04-01 05:38:53 -0700

    Update find_roots.m

--- find_roots.m
+++ find_roots.m
@@ -1,7 +1,7 @@
-function [Result] = find_roots(f,a,b)
-Result = [];
-for y= a:0.1:b
-   A = fzero(f, y);
-   Result = [Result,A];
+function [Result] = find_roots(f,a,b)  % f - Equation, a - lower bound, b - upper bound.
+Result = [];  % Holds the result.
+for y= a:0.1:b  % Iterate the given interval.
+   A = fzero(f, y);  % Use fzero to find the root.
+   Result = [Result,A];  % Insert the root to the result vector.
 end
 end
